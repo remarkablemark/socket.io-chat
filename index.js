b595cf5f6726be93eb4f59fbc81a95301fac0892
Mark <mark@remarkablemark.org>
2019-09-21 14:49:03 -0400

    feat: render `public/index.html` for GET index

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const http = require('http');
 const app = express();
 const server = http.Server(app);
 
-app.get('/', (request, response) => {
-  response.send('Hello world');
-});
+app.use(express.static('public'));
 
 const PORT = 3000;
 server.listen(PORT, () => console.log(`Listening on *:${PORT}`));
