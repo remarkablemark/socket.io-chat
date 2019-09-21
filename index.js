1023d8ac2efa6b1a0accfe312434eadb6af3ddcb
Mark <mark@remarkablemark.org>
2019-09-21 17:25:00 -0400

    feat: add socket.io to server

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const http = require('http');
+const Server = require('socket.io');
 
 const app = express();
 const server = http.Server(app);
+const io = new Server(server);
 
 app.use(express.static('public'));
 
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
 const PORT = 3000;
-server.listen(PORT, () => console.log(`Listening on *:${PORT}`));
+server.listen(PORT, () => console.log('Listening on *:' + PORT));
